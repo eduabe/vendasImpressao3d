f53efe1ba8d444897a5521f7c7747d5ac7b62b7b
Eduardo Abe <eduabe.n@gmail.com>
2026-02-01 15:53:28 -0300

    Fix SSL mode warning in PostgreSQL connection - add sslmode:require to connection config

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    sslmode: 'require'
   }
 });
 
